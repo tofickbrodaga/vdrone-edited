739c5bc504318cc7176e31456fd1a442b8cea482
tofickbrodaga <mshdtlv@gmail.com>
2025-11-21 01:35:14 +0300

    Edited

--- src/components/Scene3D/Scene3D.tsx
+++ src/components/Scene3D/Scene3D.tsx
@@ -4,18 +4,19 @@ import { OrbitControls } from '@react-three/drei';
 import { DroneConfigurator } from '../DroneConfigurator';
 import type { DroneParts } from '../DroneConfigurator';
 
+
 interface Scene3DProps {
   parts: DroneParts;
 }
 
 const Scene3D = ({ parts }: Scene3DProps) => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
+    <div style={{ width: '100%', height: '60vh' }}>
       <Canvas camera={{ position: [0, 2, 14], fov: 40 }}>
         <ambientLight intensity={1.5} />
-        <directionalLight position={[10, 10, 5]} intensity={2} />
+        <directionalLight position={[5, 10, 5]} intensity={2} />
         <Suspense fallback={null}>
-          <DroneConfigurator parts={parts} position={[0, -1.5, 0]} scale={3} />
+          <DroneConfigurator parts={parts} position={[0, -1.5, 0]} scale={1} />
           <OrbitControls
             enableZoom={true}
             enablePan={false}
