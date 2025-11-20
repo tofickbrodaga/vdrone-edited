7900ca1e1d51b41670d8ef96c4ba48f8ae7f4142
tofickbrodaga <mshdtlv@gmail.com>
2025-11-21 02:50:27 +0300

    Added dronesBlur img

--- src/pages/LendingPage/ui/DevelopSection/DevelopSection.tsx
+++ src/pages/LendingPage/ui/DevelopSection/DevelopSection.tsx
@@ -1,22 +1,29 @@
 import BlurText from "../../../../components/BlurText.tsx";
+import droneBg from "../../../../assets/dronesBlur.png"; 
 
 export const DevelopSection = () => {
     return (
         <div className='develop'>
-            <h2 className={'title'}>Мы разрабатываем решения для современных вызовов в разных областях</h2>
+            <h2 className='title'>
+                Мы разрабатываем решения для современных вызовов в разных областях
+            </h2>
+            
             <div className="realms">
                 <div className="realm">
+                    <img src={droneBg} className="drone-bg" alt="" />
                     <BlurText animateBy={'words'} direction={'top'} delay={300} text={'агропромышленные технологии'}/>
                 </div>
                 <div className="realm">
+                    <img src={droneBg} className="drone-bg" alt="" />
                     <BlurText animateBy={'words'} direction={'top'} delay={300} text={'промышленная безопасность'}/>
                 </div>
-
                 <div className="realm">
+                    <img src={droneBg} className="drone-bg" alt="" />
                     <BlurText animateBy={'words'} direction={'top'} delay={300} text={'мониторинг территорий'}/>
                 </div>
                 <div className="realm">
-                    <BlurText animateBy={'words'} direction={'top'} delay={300} text={'патрулирование протяжённых объектов'}/>
+                    <img src={droneBg} className="drone-bg" alt="" />
+                    <BlurText animateBy={'words'} direction={'top'} delay={300} text={'инспекция ЛЭП'}/>
                 </div>
             </div>
         </div>
